07d21da7b13c48b8b6a7be9c1e5a60a613ad3ebf
AshikurZaman01 <ashikurzaman774@gmail.com>
2024-02-12 16:32:19 +0600

    fixed footer

--- src/Components/Pages/Footer/Footer.jsx
+++ src/Components/Pages/Footer/Footer.jsx
@@ -3,15 +3,15 @@ import { Link } from "react-router-dom";
 const Footer = () => {
     return (
         <div className="bg-slate-950 text-white">
-            <footer >
+            <footer>
 
                 <div className=' w-10/12 mx-auto py-10 md:grid md:grid-cols-6'>
-                    <div className="col-span-2 pr-10">
+                    <div className="col-span-2 pr-16">
                         <section>
                             <h1 className="text-3xl font-semibold mb-4">CareerHub</h1>
                         </section>
                         <section>
-                            <p className="text-justify mb-5">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Harum dolorem, eos nulla vero placeat amet obcaecati fugiat iure officiis minima quam delectus quasi odit porro.</p>
+                            <p className="text-justify mb-5 text-gray-500">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Harum dolorem, eos nulla vero placeat amet obcaecati fugiat iure officiis minima quam delectus quasi odit porro.</p>
                         </section>
                         <section>
                             <img src="https://github.com/ProgrammingHero1/career-hub/blob/main/assets/icons/social.png?raw=true" alt="" />
@@ -19,8 +19,8 @@ const Footer = () => {
                     </div>
 
                     <div className="col-span-1 ml-10 my-8 md:ml-0 md:my-0">
-                        <section><h1 className="text-2xl font-semibold mb-4">Company</h1></section>
-                        <section className="flex flex-col items-start gap-y-2">
+                        <section><h1 className="text-xl font-semibold mb-4">Company</h1></section>
+                        <section className="flex flex-col items-start text-gray-500 gap-y-2">
                             <Link>About Us</Link>
                             <Link>Work</Link>
                             <Link>Latest News</Link>
@@ -29,8 +29,8 @@ const Footer = () => {
                     </div>
 
                     <div className="col-span-1 ml-10 my-8 md:ml-0 md:my-0">
-                        <section><h1 className="text-2xl font-semibold mb-4">Product</h1></section>
-                        <section className="flex flex-col items-start gap-y-2">
+                        <section><h1 className="text-xl font-semibold mb-4">Product</h1></section>
+                        <section className="flex flex-col items-start text-gray-500 gap-y-2">
                             <Link>Prototype</Link>
                             <Link>Plans & Pricing</Link>
                             <Link>Customers</Link>
@@ -39,8 +39,8 @@ const Footer = () => {
                     </div>
 
                     <div className="col-span-1 ml-10 my-8 md:ml-0 md:my-0">
-                        <section><h1 className="text-2xl font-semibold mb-4">Support</h1></section>
-                        <section className="flex flex-col items-start gap-y-2">
+                        <section><h1 className="text-xl font-semibold mb-4">Support</h1></section>
+                        <section className="flex flex-col items-start text-gray-500 gap-y-2">
                             <Link>Help Desk</Link>
                             <Link>Sales</Link>
                             <Link>Become a Partner</Link>
@@ -49,8 +49,8 @@ const Footer = () => {
                     </div>
 
                     <div className="col-span-1 ml-10 my-8 md:ml-0 md:my-0">
-                        <section><h1 className="text-2xl font-semibold mb-4">Contact</h1></section>
-                        <section className="flex flex-col items-start gap-y-2">
+                        <section><h1 className="text-xl font-semibold mb-4">Contact</h1></section>
+                        <section className="flex flex-col items-start text-gray-500 gap-y-2">
                             <Link>524 Brodway, NYC</Link>
                             <Link>+1777-978-5570</Link>
                         </section>
