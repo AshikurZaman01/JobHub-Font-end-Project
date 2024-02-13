e8d36219875e4f29df1004c76116d47b1567ed00
AshikurZaman01 <ashikurzaman774@gmail.com>
2024-02-13 16:12:24 +0600

    job details responsive

--- src/Components/FeaturesJob/JobDetails.jsx
+++ src/Components/FeaturesJob/JobDetails.jsx
@@ -1,8 +1,6 @@
 import { useLoaderData, useParams } from "react-router-dom";
 
-
 const JobDetails = () => {
-
     const data = useLoaderData();
     const { id } = useParams();
 
@@ -10,41 +8,42 @@ const JobDetails = () => {
 
     const { company_name, job_title, job_type, job_description, job_responsibility, educational_requirements, experiences, contact_information, location, logo, remote_or_onsite, salary } = findData || {};
 
-
     return (
-        <div className="w-10/12 mx-auto">
+        <div className="w-full sm:w-10/12 mx-auto">
 
-            <div className="grid grid-cols-3 gap-10">
+            <div className="grid grid-cols-1 sm:grid-cols-3 gap-5">
 
-                <section className="col-span-2">
-                    <div className="flex">
-                        <h1><span className="font-bold">Job Description :</span> <span className="text-gray-500">{job_description}</span></h1>
+                <section className="sm:col-span-2">
+                    <div className="flex flex-col">
+                        <h1 className="font-bold">Job Description:</h1>
+                        <p className="text-gray-500">{job_description}</p>
                     </div>
 
-                    <div className="flex mt-8">
-                        <h1><span className="font-bold">Job Responsibility :</span> <span className="text-gray-500">{job_responsibility}</span></h1>
+                    <div className="flex flex-col mt-8">
+                        <h1 className="font-bold">Job Responsibility:</h1>
+                        <p className="text-gray-500">{job_responsibility}</p>
                     </div>
 
                     <div className="mt-8">
-                        <h1 className="font-bold">Education Requirements : </h1>
+                        <h1 className="font-bold">Education Requirements:</h1>
                         <p className="text-gray-500">{educational_requirements}</p>
                     </div>
 
                     <div className="mb-10 mt-8">
-                        <h1 className="font-bold">Experiences : </h1>
+                        <h1 className="font-bold">Experiences:</h1>
                         <p className="text-gray-500">{experiences}</p>
                     </div>
                 </section>
 
-                <section >
-                    <div className="col-span-1 bg-blue-200 p-7 rounded">
+                <section className="flex flex-col justify-between">
+                    <div className="bg-blue-200 p-7 rounded">
                         <h1 className="font-bold">Job Details</h1>
                         <div className="divider"></div>
 
                         <div className="space-y-2">
-                            <span className="flex items-center space-x-1"><img src="https://github.com/ProgrammingHero1/career-hub/blob/main/assets/icons/money.png?raw=true" alt="" /><h1 className="font-semibold">Salary : </h1><span className="text-gray-500">{salary} (Per Months)</span></span>
+                            <span className="flex items-center space-x-1"><img src="https://github.com/ProgrammingHero1/career-hub/blob/main/assets/icons/money.png?raw=true" alt="" /><h1 className="font-semibold">Salary:</h1><span className="text-gray-500">{salary} (Per Month)</span></span>
 
-                            <span className="flex items-center space-x-1"><img src="https://github.com/ProgrammingHero1/career-hub/blob/main/assets/icons/calendar.png?raw=true" alt="" /><h1 className="font-semibold">Job Title : </h1><span className="text-gray-500">{job_title}</span></span>
+                            <span className="flex items-center space-x-1"><img src="https://github.com/ProgrammingHero1/career-hub/blob/main/assets/icons/calendar.png?raw=true" alt="" /><h1 className="font-semibold">Job Title:</h1><span className="text-gray-500">{job_title}</span></span>
 
                             <h1 className="font-bold pt-5">Contact Information</h1>
                         </div>
@@ -52,17 +51,16 @@ const JobDetails = () => {
                         <div className="divider"></div>
 
                         <div className="space-y-3">
-                            <span className="flex items-center space-x-1"><img src="https://github.com/ProgrammingHero1/career-hub/blob/main/assets/icons/phone.png?raw=true" alt="" /><h1 className="font-semibold">Phone : </h1><span className="text-gray-500">{contact_information["phone"]}</span></span>
+                            <span className="flex items-center space-x-1"><img src="https://github.com/ProgrammingHero1/career-hub/blob/main/assets/icons/phone.png?raw=true" alt="" /><h1 className="font-semibold">Phone:</h1><span className="text-gray-500">{contact_information["phone"]}</span></span>
 
-                            <span className="flex items-center space-x-1"><img src="https://github.com/ProgrammingHero1/career-hub/blob/main/assets/icons/email.png?raw=true" alt="" /><h1 className="font-semibold">Email : </h1><span className="text-gray-500">{contact_information["email"]}</span></span>
+                            <span className="flex items-center space-x-1"><img src="https://github.com/ProgrammingHero1/career-hub/blob/main/assets/icons/email.png?raw=true" alt="" /><h1 className="font-semibold">Email:</h1><span className="text-gray-500">{contact_information["email"]}</span></span>
 
-                            <span className="flex  items-center space-x-1"><img src="https://github.com/ProgrammingHero1/career-hub/blob/main/assets/icons/location2.png?raw=true" alt="" /><h1 className="font-semibold">Address : </h1><span className="text-gray-500">{contact_information["address"]}</span></span>
+                            <span className="flex  items-center space-x-1"><img src="https://github.com/ProgrammingHero1/career-hub/blob/main/assets/icons/location2.png?raw=true" alt="" /><h1 className="font-semibold">Address:</h1><span className="text-gray-500">{contact_information["address"]}</span></span>
                         </div>
                     </div>
 
-
                     <div>
-                        <button className="btn bg-purple-500 w-full text-white mt-6">Apply Now</button>
+                        <button className="btn bg-purple-500 w-full text-white mt-6 mb-20">Apply Now</button>
                     </div>
                 </section>
             </div>
