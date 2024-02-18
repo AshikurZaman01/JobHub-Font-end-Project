6d2438b7143601c896db9511fe79434a4979bd9d
AshikurZaman01 <ashikurzaman774@gmail.com>
2024-02-18 20:10:44 +0600

    functioanal filter by button

--- src/Components/Pages/AppliedJobs/AppliedJobs.jsx
+++ src/Components/Pages/AppliedJobs/AppliedJobs.jsx
@@ -6,6 +6,7 @@ import StoredJobs from "./storedJobs";
 const AppliedJobs = () => {
 
     const [storedJobId, setStoredJobId] = useState([]);
+    const [filterBy, setFilterBy] = useState([]);
     const jobsData = useLoaderData();
 
     useEffect(() => {
@@ -15,11 +16,24 @@ const AppliedJobs = () => {
         if (jobsData.length > 0) {
             const storedJobIds = jobsData.filter(job => storedJobId.includes(job.id))
             setStoredJobId(storedJobIds);
+            setFilterBy(storedJobIds);
         }
 
     }, [jobsData])
 
-
+    const handleFilter = (filter) => {
+        if (filter === 'all') {
+            setFilterBy(storedJobId);
+        }
+        else if (filter === "Remote") {
+            const remoteJobs = storedJobId.filter(job => job.remote_or_onsite === "Remote");
+            setFilterBy(remoteJobs);
+        }
+        else if (filter === "Onsite") {
+            const onsiteJobs = storedJobId.filter(job => job.remote_or_onsite === "Onsite");
+            setFilterBy(onsiteJobs);
+        }
+    }
 
     return (
         <div className="w-10/12 mx-auto">
@@ -28,15 +42,16 @@ const AppliedJobs = () => {
                 <details className="dropdown">
                     <summary className="m-1 btn">Filter by</summary>
                     <ul className="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52">
-                        <li><a>Remote</a></li>
-                        <li><a>Onsite</a></li>
+                        <li onClick={() => handleFilter('all')}><a>All</a></li>
+                        <li onClick={() => handleFilter('Remote')}><a>Remote</a></li>
+                        <li onClick={() => handleFilter('Onsite')}><a>Onsite</a></li>
                     </ul>
                 </details>
             </div>
 
             <div className="grid grid-cols-1 gap-6 mb-10">
                 {
-                    storedJobId.map(job => <StoredJobs job={job} key={job.id}></StoredJobs>)
+                    filterBy.map(job => <StoredJobs job={job} key={job.id}></StoredJobs>)
                 }
             </div>
         </div>
